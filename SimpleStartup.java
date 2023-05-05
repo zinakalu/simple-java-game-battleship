24d99c75f5e7308ddcf88294596058d603b8f94a
zinakalu <zina.kalu@gmail.com>
2023-05-05 15:15:40 -0400

    using object, instances and loops

---
+++ SimpleStartup.java
@@ -0,0 +1,24 @@
+public class SimpleStartup{
+    private int[] locationCells;
+    private int numOfHits = 0;
+
+    public void setLocationCells(int[] locs){
+        locationCells = locs;
+    }
+    public String checkYourself(int guess){
+        String result = "miss";
+
+        for (int cell : locationCells){
+            if (guess == cell){
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+            if (numOfHits ==locationCells.length){
+                result = "kill";
+            }
+        System.out.println(result);
+    return result;
+    }
+}
